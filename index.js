6d2e9d029d58fca64f84c797d2b3f2b330c8b412
WollerChris <99303783+WollerChris@users.noreply.github.com>
2023-05-23 16:30:24 -0600

    Archive list is completed
    After doing thorough research i was able to move from current task list to finished once completed.   The Archive will display the task associated and date completed.

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ function addItem() {
     el.className = 'list-group-item';
     document.getElementById('list').append(el);
     el.addEventListener('click', done);
-
-
     event.preventDefault();
 
 
@@ -26,19 +24,10 @@ function removeItem() {
 function done() {
     this.className = "done";
     this.removeEventListener('click',done);
- 
-
-    var el = document.createElement('li')
-    var val = document.getElementsByClassName("done").value;
-    el.innerHTML = val;
-    el.className = 'list-group-item';
-    document.getElementById('archive').append(el);
-
     this.append(complete);
 
   }
   
-  // Initialize all listener for current undone tasks
 function init() {
     var undoneItems = document.getElementsByClassName('tasks');
     for(var i = 0; i < undoneItems.length; i++){
@@ -46,5 +35,16 @@ function init() {
     }
   }
 
+  list.addEventListener('click', function(e){
+    if(e.target != this){
+      archive.appendChild(e.target.cloneNode(true));
+      e.target.remove()
+    }
+  })
   
-
+  archive.addEventListener('click', function(e){
+    if(e.target != this){
+      list.appendChild(e.target.cloneNode(true));
+      e.target.remove()
+    }
+  })
